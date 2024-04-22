2c06d0d03ce059fb1bde9d7ad0a6c0328778d229
jventural <jventuraleon@gmail.com>
2024-04-22 06:00:26 -0500

    third commit

--- R/loading_extract.R
+++ R/loading_extract.R
@@ -6,22 +6,19 @@ loading_extract <- function(results) {
   if (!requireNamespace("dplyr", quietly = TRUE)) {
     stop("Por favor instala el paquete 'dplyr'")
   }
+  if (!requireNamespace("pbapply", quietly = TRUE)) {
+    stop("Por favor instala el paquete 'pbapply'")
+  }
 
   # Asignar results$Fits a fit_list para simplificar el acceso
   fit_list <- results$Fits
 
-  # Lista para almacenar los resultados procesados
-  results_list <- list()
-
-  # Bucle que recorre cada elemento de fit_list y aplica las operaciones
-  for (i in seq_along(fit_list)) {
-    results <- lavaan::standardizedsolution(fit_list[[i]]) %>%
+  # Usar pblapply para procesar cada ajuste en fit_list con una barra de progreso
+  results_list <- pblapply(fit_list, function(fit) {
+    lavaan::standardizedsolution(fit) %>%
       dplyr::select(lhs, op, est.std, rhs) %>%
       dplyr::filter(op == "=~")
-
-    # Guardar los resultados en la lista
-    results_list[[i]] <- results
-  }
+  })
 
   # Combinar todos los dataframes almacenados en la lista en un único dataframe
   final_results <- dplyr::bind_rows(results_list)
